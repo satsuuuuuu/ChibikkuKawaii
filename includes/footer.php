8f152ce63932e371b0cff547f91fcd73cb247413
satsuuuuuu <satsu221@gmail.com>
2024-12-17 04:21:35 +0800

    dsad

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
 <!-- includes/footer.php -->
 <footer class="footer">
     <p>&copy; <?php echo date("Y"); ?> Chibikku Kawaii. All rights reserved.</p>
+    <button onclick="window.location.href='orders.php';"
+    class="py-4 px-6 hover:bg-[#F78FAF] transition">orders</button>
 </footer>
